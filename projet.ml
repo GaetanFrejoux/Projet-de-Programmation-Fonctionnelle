cec6b80b940957cc484bba70243774a4c5771c1e
Mathieu Niord <69221076+MathieuNiord@users.noreply.github.com>
2021-03-13 11:11:39 +0100

    Correction commentaires et indentation
    J'ai corrigé les commentaires, les erreurs et l'indentation

--- projet.ml
+++ projet.ml
@@ -15,9 +15,6 @@ open Stack;; (*Module Pile*)
 #show Stack;;
 
 
-
-
-
 (*Types*)
 
 (*operator*)
@@ -32,17 +29,13 @@ type tree =
 ;;
 
 
-
-
 (*tests*)
 let x = string_to_token_list " 34 ~ 56 2 + x * -;";;
 
 
 (*Analyse lexicale*)
 
-(*Cette partie est donnÃ©e dans le sujet en utilisant le module expression_scanner*)
-
-
+(*Cette partie est donnée dans le sujet en utilisant le module expression_scanner*)
 
 
 
@@ -60,71 +53,62 @@ let tokenToOperator token =
   | Subtract -> Minus
   | Multiply -> Mult
   | Divide -> Div
-  | _ -> failwith" It's not an operator."
+  | _ -> failwith "--- Error : It's not an operator ---"
 ;;
 
 (*fonction qui transforme une liste de token en un arbre de syntaxe abstraite*)
 let parse list =
   
-  let ended = ref false and (*booleen qui dit si l'expression s'est terminÃ© par ';' *)
+  let ended = ref false and (*booleen qui indique si l'expression s'est terminée par ';' *)
       stack_len = ref 0 and (*Longueur de la pile*)
-      error = "This expression is not valid." in (*Message d'erreur*)
+      error = "--- Invalid Expression ---" in (*Message d'erreur*)
   
   let rec parse_aux list stack = (*Fonction anonyme*)
-    if(!stack_len<0) 
+    if(!stack_len < 0) 
     then failwith error
     else
       (
         match list with
           
-        | [] ->
-           if (!ended && !stack_len = 1) (*Test finir par End et taille = 1*)
-           then ()
-           else failwith error
-          
-        | hd::tl ->
-           ignore(
-               match hd with
+        |[] -> if (!ended && !stack_len = 1) (*Test finir par End et taille = 1*)
+               then ()
+               else failwith error
+         
+        |hd::tl -> ignore(
+                       match hd with
 
-               | Variable(v) -> (*Cas variable*)
-                  
-                  stack_len := !stack_len + 1;
-                  push (Var(v)) stack
-                  
-               | Number(n) -> (*Cas nombre*)
-                  
-                  stack_len := !stack_len + 1;
-                  push (Cst(n)) stack;
-                  
-               | Minus -> (*Cas de l'operateur unaire*)
-                  
-                  if(!stack_len<1)
-                  then failwith error
-                  else push (Unary(pop stack)) stack
-                 
+                       (*Cas variable*)
+                       |Variable(v) -> stack_len := !stack_len + 1;
+                                       push (Var(v)) stack
 
-               | End -> (*Cas de la fin de l'expression*)
-                  
-                  if(tl=[])
-                  then ended := true
-                  else failwith error
-                 
-               | _ -> (* Cas des operateurs *)
-                  
-                  if(!stack_len<2) (*test si il y a le fils gauche et droit*)
-                  then failwith error
-                  else
-                    (
-                      stack_len := !stack_len - 1; (*retire 2 élément et en rajoute 1 donc -1*)
-                      let d = pop stack in (*fils droit*)
-                      let g = pop stack in (*fils gauche*)
-                      let op = tokenToOperator hd in (*opeateur*)
-                      
-                      push (Binary(op,g,d)) stack 
+                       (*Cas nombre*)
+                       |Number(n) -> stack_len := !stack_len + 1;
+                                     push (Cst(n)) stack;
 
-                    )
-             );
-           parse_aux tl stack
+                       (*Cas de l'operateur unaire*)
+                       |Minus -> if(!stack_len<1)
+                                 then failwith error
+                                 else push (Unary(pop stack)) stack
+                        
+                       (*Cas de la fin de l'expression*)
+                       |End -> if(tl=[])
+                               then ended := true
+                               else failwith error
+                             
+                       (* Cas des operateurs *)
+                       |_ -> if(!stack_len<2) (*test si il y a le fils gauche et droit*)
+                             then failwith error
+                             else
+                               (
+                                 stack_len := !stack_len - 1; (*retire 2 élément et en rajoute 1 donc -1*)
+                                 let d = pop stack in (*fils droit*)
+                                 let g = pop stack in (*fils gauche*)
+                                 let op = tokenToOperator hd in (*opeateur*)
+                                 
+                                 push (Binary(op,g,d)) stack
+                               )
+                     );
+                   parse_aux tl stack
       )
   in
   let stack = create() in (*Pile vide*)
@@ -136,9 +120,6 @@ let ans = parse x;;
 
 
 
-
-
-
 (*Simplication sur l'arbre*)
 
 (*fonction qui simplifie un arbre de syntaxe abstraite*)
@@ -147,9 +128,9 @@ let ans = parse x;;
   -  une sous-expression constituée exclusivement de constantes.
 
   -  une expression de la forme 1*x ou 0 + x sera simplifiée en x ; de même l'expression 0 * x
-     sera simplifié par 0
+     sera simplifiée par 0
 
-  -  une sous-expression de la forme x * x sera simplifiée en 0 
+  -  une sous-expression de la forme 0 * x sera simplifiée en 0 
 
   -  une sous-expression de la forme x/x sera simplifiée en 1
  *)
@@ -164,7 +145,7 @@ let simplifyTree tree =
 
 
 
-(*Affichage du rÃ©sultat*)
+(*Affichage du résultat*)
 
 (*TODO*)
 
@@ -172,5 +153,5 @@ let simplifyTree tree =
 (*Dans cette fonction d'affichage, il faudra afficher l'expression avec le moins de parenthèses
  possible.
  
-  -  Ne pas mettre de parenthèse lorsqu'il y a associativité : ((a*b)*c)*(e+f) doit être a*b*c*(e+f)
+  -  Ne pas mettre de parenthèse lorsqu'il y a association : ((a*b)*c)*(e+f) doit être a*b*c*(e+f)
  *)
